2ed88a546f13a2a04f326278c94fda244e5ed8f3
MalteAtCodiga <malte@code-inspector.com>
2022-10-14 10:03:53 +0200

    Add indention utils and unit tests

--- src/Extension/SnippetFormats/EditorUtils.cs
+++ src/Extension/SnippetFormats/EditorUtils.cs
@@ -45,5 +45,70 @@ namespace Extension.SnippetFormats
 
 			return keywords.Length >= 2;
 		}
+
+		/// <summary>
+		/// Returns a new indented code block based on the given settings.
+		/// The first line of the code block is expexted to be indented already.
+		/// </summary>
+		/// <param name="code"></param>
+		/// <param name="indentSize"></param>
+		/// <param name="tabSize"></param>
+		/// <param name="useSpace"></param>
+		/// <returns></returns>
+		public static string IndentCodeBlock(string code, int indentLevel, int indentSize, int tabSize, bool useSpace)
+		{
+			var lines = code.Split('\n');
+			string spaceIndent = new string(' ', indentSize);
+			string tabIndent = new string('\t', indentSize / tabSize);
+			string finalIndent;
+
+			if (useSpace)
+			{
+				finalIndent = string.Concat(Enumerable.Repeat(spaceIndent, indentLevel));
+			}
+			else
+			{
+				var tabs = string.Concat(Enumerable.Repeat(tabIndent, indentLevel));
+				string spaces = "";
+
+				if(tabSize > indentSize)
+					spaces = string.Concat(Enumerable.Repeat(spaceIndent, indentLevel));
+
+				finalIndent = tabs + spaces;
+			}
+
+			for (int i = 1; i < lines.Length; i++)
+			{
+				lines[i - 1] = lines[i - 1] + "\n";
+				lines[i] = lines[i].Insert(0, finalIndent);
+			}
+
+			var indentedCode = string.Concat(lines);
+			return indentedCode;
+		}
+
+		public static int GetIndentLevel(string line, int indentSize, int tabSize, bool useSpace)
+		{
+			//get non text chararcters
+			var firstCharIndex = line.IndexOf(line.Trim().First());
+			var indentChars = line.Substring(0, firstCharIndex);
+			var charCounts = indentChars.GroupBy(c => c)
+				.Select(c => new { Char = c.Key, Count = c.Count() });
+			
+			int tabCount = charCounts.SingleOrDefault(c => c.Char == '\t')?.Count ?? 0;
+			int spaceCount = charCounts.SingleOrDefault(c => c.Char == ' ')?.Count ?? 0;
+
+			int indentLevel = 0;
+			if (useSpace)
+			{
+				indentLevel = spaceCount / indentSize;
+			}
+			else
+			{
+				indentLevel = (tabCount * tabSize + spaceCount) / indentSize;
+			}
+
+			return indentLevel;
+		}
 	}
 }
--- src/Tests/EditorUtilsTest.cs
+++ src/Tests/EditorUtilsTest.cs
@@ -44,5 +44,108 @@ namespace Tests
 			// act & assert
 			return EditorUtils.IsSemanticSearchComment(line);
 		}
+
+		[Test]
+		// use tabs
+		[TestCase("\t\tpublic void main()", 4, 4, false, ExpectedResult = 2)]
+		[TestCase("\tpublic void main()", 4, 4, false, ExpectedResult = 1)]
+		[TestCase("\tpublic void main()", 2, 4, false, ExpectedResult = 2)]
+		[TestCase("\t\tpublic void main()", 2, 4, false, ExpectedResult = 4)]
+		[TestCase("\tpublic void main()", 2, 8, false, ExpectedResult = 4)]
+		[TestCase("\tpublic void main()", 8, 4, false, ExpectedResult = 0)]
+		[TestCase("\tpublic void main()", 4, 8, false, ExpectedResult = 2)]
+		// use space
+		[TestCase("        public void main()", 4, 4, true, ExpectedResult = 2)]
+		[TestCase("    public void main()", 4, 4, true, ExpectedResult = 1)]
+		[TestCase("    public void main()", 2, 4, true, ExpectedResult = 2)]
+		[TestCase("        public void main()", 2, 4, true, ExpectedResult = 4)]
+		[TestCase("        public void main()", 2, 8, true, ExpectedResult = 4)]
+		// mixed
+		[TestCase("\tpublic void main()", 4, 8, false, ExpectedResult = 2)]
+		[TestCase("\t    public void main()", 4, 8, false, ExpectedResult = 3)]
+		[TestCase("\t     public void main()", 4, 8, false, ExpectedResult = 3)]
+		public int GetIndentLevel_should_return_indent_level_based_on_config(string line, int indentSize, int tabSize, bool useSpace)
+		{
+			// act
+			var level = EditorUtils.GetIndentLevel(line, indentSize, tabSize, useSpace);
+
+			// assert
+			return level;
+		}
+
+		[Test]
+		public void IndentCodeBlock_should_retain_existing_indentation_using_tabs()
+		{
+			// arrange
+			var codeBlock =
+				"\tpublic void main()\n" +
+				"{\n" +
+				"\t\n" +
+				"}";
+																																									
+			// act
+			var indented = EditorUtils.IndentCodeBlock(
+				code: codeBlock, 
+				indentLevel: 1, 
+				indentSize: 4, 
+				tabSize: 4, 
+				useSpace: false);
+
+			// assert
+			Assert.That(indented, Is.EqualTo("\tpublic void main()\n" +
+											 "\t{\n" +
+											 "\t\t\n" +
+											 "\t}"));
+		}
+
+		[Test]
+		public void IndentCodeBlock_should_retain_existing_indentation_using_whitespaces()
+		{		
+			// arrange
+			var codeBlock =
+				"    public void main()\n" +
+				"{\n" +
+				"    \n" +
+				"}";
+
+			// act
+			var indented = EditorUtils.IndentCodeBlock(
+				code: codeBlock,
+				indentLevel: 1,
+				indentSize: 4,
+				tabSize: 4,
+				useSpace: true);
+
+			// assert
+			Assert.That(indented, Is.EqualTo("    public void main()\n" +
+											 "    {\n" +
+											 "        \n" +
+											 "    }"));
+					}
+
+		[Test]
+		public void IndentCodeBlock_should_retain_existing_indentation_using_small_indent_size()
+		{
+			// arrange
+			var codeBlock =
+				"  public void main()\n" +
+				"{\n" +
+				"  \n" +
+				"}";
+
+			// act
+			var indented = EditorUtils.IndentCodeBlock(
+				code: codeBlock,
+				indentLevel: 1,
+				indentSize: 2,
+				tabSize: 4,
+				useSpace: false);
+
+			// assert
+			Assert.That(indented, Is.EqualTo("  public void main()\n" +
+											 "  {\n" +
+											 "    \n" +
+											 "  }"));
+		}
 	}
 }

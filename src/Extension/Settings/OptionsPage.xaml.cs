a6e03ba46c8be778d13994d107b2ff48a5ed9dfc
MalteAtCodiga <malte@code-inspector.com>
2022-10-18 13:08:17 +0200

    Remove unused references

--- src/Extension/Settings/OptionsPage.xaml.cs
+++ src/Extension/Settings/OptionsPage.xaml.cs
@@ -1,21 +1,8 @@
-﻿using Extension.Caching;
-using GraphQLClient;
+﻿using GraphQLClient;
 using Microsoft.VisualStudio.Shell;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using static Extension.Settings.OptionsPageProvider;
 
 namespace Extension.Settings
 {

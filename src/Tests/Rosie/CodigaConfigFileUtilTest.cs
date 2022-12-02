edf02708edd975445d1dd096a0138fe2fa8dc827
Tamas Balog <picimako@protonmail.com>
2022-12-02 10:00:13 +0100

    Add further tests for CodigaConfigFileUtil

--- src/Tests/Rosie/CodigaConfigFileUtilTest.cs
+++ src/Tests/Rosie/CodigaConfigFileUtilTest.cs
@@ -1,4 +1,7 @@
+using System.IO;
+using EnvDTE;
 using Extension.Rosie;
+using Moq;
 using NUnit.Framework;
 
 namespace Tests.Rosie
@@ -9,34 +12,37 @@ namespace Tests.Rosie
     [TestFixture]
     internal class CodigaConfigFileUtilTest
     {
+        private string _solutionDir;
+        private string _codigaConfigFile;
+
         #region DeserializeConfig negative cases
-        
+
         [Test]
         public void DeserializeConfig_should_return_null_for_null_raw_config()
         {
             var codigaConfigFile = CodigaConfigFileUtil.DeserializeConfig(null);
-            
+
             Assert.That(codigaConfigFile, Is.Null);
         }
-        
+
         [Test]
         public void DeserializeConfig_should_return_null_for_empty_raw_config()
         {
             var codigaConfigFile = CodigaConfigFileUtil.DeserializeConfig("");
-            
+
             Assert.That(codigaConfigFile, Is.Null);
         }
-        
+
         [Test]
         public void DeserializeConfig_should_return_null_for_whitespace_only_raw_config()
         {
             var codigaConfigFile = CodigaConfigFileUtil.DeserializeConfig("   ");
-            
+
             Assert.That(codigaConfigFile, Is.Null);
         }
-        
+
         #endregion
-        
+
         #region CollectRulesetNames positive cases
 
         [Test]
@@ -117,38 +123,79 @@ rulesets:
 not-rulesets:
   - my-csharp-ruleset
   - my-other-ruleset");
-        
+
             var rulesetNames = CodigaConfigFileUtil.CollectRulesetNames(codigaConfigFile);
-        
+
             Assert.That(rulesetNames, Is.Empty);
         }
-        
+
         [Test]
         public void CollectRulesetNames_should_return_no_ruleset_name_for_non_sequence_empty_rulesets_list()
         {
             var codigaConfigFile = CodigaConfigFileUtil.DeserializeConfig(@"
 rulesets:
   ");
-        
+
             var rulesetNames = CodigaConfigFileUtil.CollectRulesetNames(codigaConfigFile);
-        
+
             Assert.That(rulesetNames, Is.Empty);
         }
-        
+
         [Test]
         public void CollectRulesetNames_should_return_no_ruleset_name_for_non_sequence_rulesets_property()
         {
             var codigaConfigFile = CodigaConfigFileUtil.DeserializeConfig(@"
 rulesets:
   rules:");
-        
+
             var rulesetNames = CodigaConfigFileUtil.CollectRulesetNames(codigaConfigFile);
-        
+
             Assert.That(rulesetNames, Is.Empty);
         }
 
         #endregion
-        
+
+        #region FindCodigaConfigFile
+
+        [Test]
+        public void FindCodigaConfigFile_should_return_null_for_missing_solution_root()
+        {
+            var solution = new Mock<Solution>();
+
+            var configFile = CodigaConfigFileUtil.FindCodigaConfigFile(solution.Object);
+
+            Assert.That(configFile, Is.Null);
+        }
+
+        [Test]
+        public void FindCodigaConfigFile_should_return_null_for_missing_codiga_config_file()
+        {
+            var solution = new Mock<Solution>();
+            _solutionDir = Path.GetTempPath();
+            solution.Setup(s => s.FullName).Returns(_solutionDir);
+
+            var configFile = CodigaConfigFileUtil.FindCodigaConfigFile(solution.Object);
+
+            Assert.That(configFile, Is.Null);
+        }
+
+        [Test]
+        public void FindCodigaConfigFile_should_return_path_of_codiga_config_file()
+        {
+            _solutionDir = Path.GetTempPath();
+            _codigaConfigFile = $"{_solutionDir}codiga.yml";
+
+            var solution = new Mock<Solution>();
+            solution.Setup(s => s.FullName).Returns(_solutionDir);
+            using var fs = File.Create(_codigaConfigFile);
+
+            var configFile = CodigaConfigFileUtil.FindCodigaConfigFile(solution.Object);
+
+            Assert.That(configFile, Is.EqualTo(_codigaConfigFile));
+        }
+
+        #endregion
+
         #region Invalid ruleset names
 
         [Test]
@@ -179,7 +226,16 @@ rulesets:
         {
             return CodigaConfigFileUtil.IsRulesetNameValid(rulesetName);
         }
-        
+
         #endregion
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (File.Exists(_solutionDir))
+                File.Delete(_solutionDir);
+            if (File.Exists(_codigaConfigFile))
+                File.Delete(_codigaConfigFile);
+        }
     }
 }
